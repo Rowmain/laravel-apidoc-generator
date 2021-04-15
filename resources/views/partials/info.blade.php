8adc8a81d39c5b10fb87f1927667a3d937f57fff
Dalleau Romain <dalleau.romain@gmail.com>
2021-04-15 15:22:02 +0400

    Update info.blade.php

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,6 +1,7 @@
-# Info
-
-Welcome to the generated API reference.
-@if($showPostmanCollectionButton)
-[Get Postman Collection]({{url($outputPath.'/collection.json')}})
+@if(empty(config('apidoc.hide_info')) || config('apidoc.hide_info') === false) 
+    # Info
+    {{config('apidoc.info_blade', 'Welcome to the generated API reference.')}}
+    @if($showPostmanCollectionButton)
+    [Get Postman Collection]({{url($outputPath.'/collection.json')}})
+    @endif
 @endif
